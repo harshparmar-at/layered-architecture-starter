7ad03da870386d168cf82e02594e40bd62012720
harshparmar-at <harsh.parmar@avestatechnologies.com>
2024-01-31 19:18:05 +0530

    ..

--- src/infrastructure/database/sequelize/models/userModel.ts
+++ src/infrastructure/database/sequelize/models/userModel.ts
@@ -1,32 +1,3 @@
-// import { Model, DataTypes } from "sequelize";
-// import sequelize from "../config/createConnection";
-
-// export class UserModel extends Model {}
-
-// UserModel.init(
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       autoIncrement: true,
-//       primaryKey: true,
-//     },
-//     name: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     city: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     birthDate: {
-//       type: DataTypes.DATE,
-//       allowNull: false,
-//     },
-//   },
-//   { tableName: "users", sequelize, timestamps: false }
-// );
-
 import { Model, DataTypes } from "sequelize";
 import sequelize from "../config/createConnection";
 
